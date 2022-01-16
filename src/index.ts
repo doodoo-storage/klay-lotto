9855209d9f1f1bb4599cd35d6a854e3621cbedb1
keunhan lee <restart.doodoo@gmail.com>
2022-01-16 20:05:52 +0900

    :chore remove git cached

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import 'dotenv/config';
 
 import express from 'express';
 import env from 'env-var';
+import Caver from 'caver-js';
 
 import { endLotteryScheduler } from './scheduler';
+import { getCaverContract } from './contract/lottery-contract-test';
 
 const PORT = env.get('PORT').required().asPortNumber();
+const ENDPOINT_NODE_URL = env.get('ENDPOINT_NODE_URL').required().asString();
+const WALLET_ADDRESS = env.get('WALLET_ADDRESS').required().asString();
+const WALLET_PRIVATE_KEY = env.get('WALLET_PRIVATE_KEY').required().asString();
 
 (async () => {
   endLotteryScheduler();
@@ -15,6 +20,18 @@ const PORT = env.get('PORT').required().asPortNumber();
   app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
   });
+
+  app.get('/test', async (req, res) => {
+    const caver = new Caver(ENDPOINT_NODE_URL);
+
+    // @ts-ignore
+    await caver.wallet.keyring.create(WALLET_ADDRESS, WALLET_PRIVATE_KEY);
+    
+    await getCaverContract().methods.endLottery(1).send({
+      from: WALLET_ADDRESS,
+      gas: 10000000000
+    });
+  })
 
   app.listen(PORT, () => { console.log('app started') });
 })();
--- src/scheduler.ts
+++ src/scheduler.ts
@@ -5,16 +5,16 @@ import { getCaverContract } from './contract/lottery-contract-test';
 
 const ENDPOINT_NODE_URL = env.get('ENDPOINT_NODE_URL').required().asString();
 const WALLET_ADDRESS = env.get('WALLET_ADDRESS').required().asString();
-const PRIVATE_KEY = env.get('PRIVATE_KEY').required().asString();
+const WALLET_PRIVATE_KEY = env.get('WALLET_PRIVATE_KEY').required().asString();
 
 export const endLotteryScheduler = () => 
   cron.schedule('0 0 0 * * *', async () => {
     const caver = new Caver(ENDPOINT_NODE_URL);
 
     // @ts-ignore
-    await caver.wallet.keyring.create(WALLET_ADDRESS, PRIVATE_KEY);
+    await caver.wallet.keyring.create(WALLET_ADDRESS, WALLET_PRIVATE_KEY);
     
-    await getCaverContract().methods.joinLottery().send({
+    await getCaverContract().methods.endLottery().send({
       from: WALLET_ADDRESS,
       gas: 10000000000,
       value: 10**18
